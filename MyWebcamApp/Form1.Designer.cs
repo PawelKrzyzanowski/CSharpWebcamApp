897885a1795b4952a91f1d94833d6e33ffb33bc5
PawelKrzyzanowski <p.a.krzyzanowski@gmail.com>
2021-11-18 23:00:45 +0100

    Initial commit

---
+++ MyWebcamApp/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MyWebcamApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(214, 24);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(365, 24);
+            this.cboCamera.TabIndex = 0;
+            this.cboCamera.SelectedIndexChanged += new System.EventHandler(this.cboCamera_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera:";
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(134, 72);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(445, 320);
+            this.picBox.TabIndex = 2;
+            this.picBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(320, 411);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 529);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboCamera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ MyWebcamApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+
+namespace MyWebcamApp
+{
+    public partial class Form1 : Form
+    {
+        /* FIELDS */
+        FilterInfoCollection filterInfoCollection; /* enumerates video devices */
+        VideoCaptureDevice videoCaptureDevice; /* Class for local video capture devices like webcam */
+
+        /* CONSTRUCTORS */
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* OTHER METHODS */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* identify the camera at start */
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
+            /* update combobox */
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboCamera.Items.Add(filterInfo.Name);
+            cboCamera.SelectedIndex = 0;
+
+            /* In Microsoft COM, a moniker is an object that provides a pointer to the object it identifies.  */
+            string deviceMoniker = filterInfoCollection[cboCamera.SelectedIndex].MonikerString; 
+            /* select the camera */
+            videoCaptureDevice = new VideoCaptureDevice(deviceMoniker);
+            /* start aquisition */
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            picBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void cboCamera_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+                videoCaptureDevice.Stop();
+        }
+    }
+}
